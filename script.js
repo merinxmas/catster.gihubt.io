29e7657fec6b2b3548ee778da8d95aa0da603257
Marina Mas Argemí <marina@Mac.station>
2025-12-17 22:11:41 +0100

    scrip.js changes

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const params = new URLSearchParams(window.location.search);
 const songId = params.get("song");
 
 const playerDiv = document.getElementById("player");
-playerDiv.innerHTML = ""; // IMPORTANT: neteja abans
 
 if (songId) {
-  const iframe = document.createElement("iframe");
-  iframe.src = `https://open.spotify.com/embed/track/${songId}?theme=0`;
-  iframe.width = "300";
-  iframe.height = "80";
-  iframe.frameBorder = "0";
-  iframe.allow =
-    "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
-
-  playerDiv.appendChild(iframe);
+  playerDiv.innerHTML = `
+    <div class="player-wrapper">
+      <iframe
+        src="https://open.spotify.com/embed/track/${songId}?theme=0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
+      </iframe>
+      <div class="overlay"></div>
+    </div>
+  `;
 }
